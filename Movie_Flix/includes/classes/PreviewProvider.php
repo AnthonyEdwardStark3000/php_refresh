268f014df53756203336997c4e344d8ca30f0be4
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-07-04 22:06:46 +0530

    Some more steps for entity

---
+++ Movie_Flix/includes/classes/PreviewProvider.php
@@ -0,0 +1,37 @@
+<?php
+class PreviewProvider{
+    private $con;
+    private $username;
+
+    public function __construct($con, $username){
+        $this->con = $con;
+        $this->username = $username;
+    }   
+
+    public function createPreviewVideo($entity){
+        if($entity ==null){
+            $entity = $this->getRandomEntity();
+        }
+
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $preview = $entity->getPreview();
+        $thumbnail = $entity->getThumbnail();
+        
+        return "<div class='previewContainer'>
+        <img src='$thumbnail' class='imagePreview'>
+        <video autoplay class='previewVideo'>
+        <source src='$preview' type='video/mp4'>
+        </video>
+        </div>";
+    }
+
+    private function getRandomEntity(){
+        $query = $this-> con ->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query -> execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return new Entity ($this->con, $row);
+    }
+}
+?>
--- Movie_Flix/index.php
+++ Movie_Flix/index.php
@@ -1,7 +1,13 @@
 <?php
   require_once("includes/config.php");
+  require_once("includes/classes/PreviewProvider.php");
+  require_once("includes/classes/Entity.php");
   if(!isset($_SESSION["userLoggedIn"]))
   {
     header("Location: register.php");
   }
+
+  $userLoggedIn = $_SESSION["userLoggedIn"];
+  $preview = new PreviewProvider($con, $userLoggedIn);
+  $preview -> createPreviewVideo(null);
 ?>
